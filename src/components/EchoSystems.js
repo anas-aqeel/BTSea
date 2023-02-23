8f1cf2d51e428a5a5b2de0d55b7ebd5de27f9ec0
unknown <anas.452111@gmail.com>
2023-02-23 21:24:51 +0500

    UI updated

--- src/components/EchoSystems.js
+++ src/components/EchoSystems.js
@@ -6,21 +6,37 @@ const EchoSystems = () => {
       <h1 className="text-white mt-32 mb-14 font-primary font-[700] text-4xl leading-[46px] text-center">
         BTSEA ECHOSYSTEM
       </h1>
-      <div className="flex justify-between mb-32 mx-4">
+      <div className="flex justify-between  mx-4">
         <div className="h-72 w-72 bg-slate-700 rounded-lg flex flex-col items-center justify-center">
-
+          <div className="h-20 w-20 rounded-full shadow-lg mb-14">h</div>
+          <div className="text-white text-lg px-10 text-center">
+            Ordinal exclusive NFT marketplace
+          </div>
         </div>
         <div className="h-72 w-72 bg-slate-700 rounded-lg flex flex-col items-center justify-center">
-
+          <div className="h-20 w-20 rounded-full shadow-lg mb-14">h</div>
+          <div className="text-white text-lg px-10 text-center">
+            Ordinal exclusive NFT marketplace
+          </div>
         </div>
         <div className="h-72 w-72 bg-slate-700 rounded-lg flex flex-col items-center justify-center">
-
+          <div className="h-20 w-20 rounded-full shadow-lg mb-14">h</div>
+          <div className="text-white text-lg px-10 text-center">
+            Ordinal exclusive NFT marketplace
+          </div>
         </div>
         <div className="h-72 w-72 bg-slate-700 rounded-lg flex flex-col items-center justify-center">
-
+          <div className="h-20 w-20 rounded-full shadow-lg mb-14">h</div>
+          <div className="text-white text-lg px-10 text-center">
+            Ordinal exclusive NFT marketplace
+          </div>
         </div>
         
       </div>
+      <h1 className="text-white mt-14 mb-32 font-primary font-[700] text-xl leading-[46px] text-center">
+          EXPLORE EACH INSCRIPTION AND ORDINAL EXACTLY LIKE YOU WOULD ON
+          BITCOIN'S BLOCKCHAIN
+        </h1>
     </>
   );
 };
